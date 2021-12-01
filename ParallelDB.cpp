5d2864566199c957a62a471f504ae3f2c3fe49d9
Justin Angelel <jra221@lehigh.edu>
2021-11-30 20:02:34 -0500

    DB file w/ headers

---
+++ ParallelDB.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <stdlib>
+#include <stdio>
+
+using namespace std;
+
+class Database{
+    private:
+    unordered_hashmap<string,void*> DB;
+
+    public:
+
+    bool insert_seq(string table, int num, void* records); 
+    bool insert_data_parallel(string table, int num, void* records);
+
+    bool remove_seq(string table, int num, void* records);
+    bool remove_data_parallel(string table, int num, void* records);
+
+    bool get_seq(string table, void* conditions, void* sort, bool ascend = true); 
+    bool get_data_parallel(string table, void* conditions, void* sort, bool ascend = true);
+    bool get_task_parallel(string table, void* condistions, void* sort, bool ascend = true);
+    bool get_task_data(string table, void* condistions, void* sort, bool ascend = true);
+    
+
+
+}
