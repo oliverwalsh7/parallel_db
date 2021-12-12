f7aa738d06f42aabbea468534c631eeeb2ee4021
Justin Angelel <jangelel17@icloud.com>
2021-12-12 16:01:06 -0500

    Inserting only 256 lines

--- src/SequentialDB.cpp
+++ src/SequentialDB.cpp
@@ -11,6 +11,7 @@
 #include <algorithm> // sort
 #include <unordered_map>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -19,61 +20,74 @@ class Seq_Database {
     // Outer Level Hashmap of tables
     //   Second Level Hashmap of records in "table"
     //     Third Level Hashmap where each map is a record
-    unordered_map<string, unordered_map<string,void*>>* DB;
+    unordered_map<string, unordered_map<string,unordered_map<string,string>>>* DB;
     //unordered_map<string, unordered_map<string, unordered_map<string,void*>>>* DB;
     //string sortKey;
 
-    class compObj{
-        private:
-        string sortKey;
-        bool ascend = true;
-        public:
-        compObj(string sort, bool asc){
-            sortKey = sort;
-            ascend = asc;
-        }
-        bool operator()(void* A, void* B){
-            unordered_map<string, void*> X = *((unordered_map<string, void*>*) A);
-            unordered_map<string, void*> Y = *((unordered_map<string, void*>*) B);
-            if(X[sortKey] < Y[sortKey]){
-                if(ascend){
-                    return true;
-                }
-                return false;
-            }
-            if(ascend){
-                return false;
-            }
-            return true;
-        }
-    };
+    // class compObj{
+    //     private:
+    //     string sortKey;
+    //     bool ascend = true;
+    //     public:
+    //     compObj(string sort, bool asc){
+    //         sortKey = sort;
+    //         ascend = asc;
+    //     }
+    //     bool operator()(unordered_map<string, string> A, unordered_map<string, string> B){
+    //         unordered_map<string, void*> X = *((unordered_map<string, void*>*) A);
+    //         unordered_map<string, void*> Y = *((unordered_map<string, void*>*) B);
+    //         if(X[sortKey] < Y[sortKey]){
+    //             if(ascend){
+    //                 return true;
+    //             }
+    //             return false;
+    //         }
+    //         if(ascend){
+    //             return false;
+    //         }
+    //         return true;
+    //     }
+    // };
 
     public:
 
     Seq_Database(){
-        DB = new unordered_map<string, unordered_map<string,void*>>();
+        DB = new unordered_map<string, unordered_map<string,unordered_map<string,string>>>();
         //sortKey = "";
     }
 
     //Change to bool/int for failed insertions?
-    void insert(string table, int num, vector<string> keys, vector<void*> records){
-        unordered_map<string,unordered_map<string, void*>> database = *DB;
+    void insert(string table, int num, vector<string> keys, vector<unordered_map<string,string>> records){
+        unordered_map<string,unordered_map<string, unordered_map<string,string>>> database = *DB;
         try {
             DB->at(table);
         } catch (const std::out_of_range& oor) {
-            unordered_map<string, void*>* x = new unordered_map<string, void*>();
+            unordered_map<string, unordered_map<string,string>>* x = new unordered_map<string, unordered_map<string,string>>();
             database[table] = *x;
+            cout << "Table doesnt exist" << endl;
             //DB->insert(std::make_pair<string, unordered_map<string, void*>(table, new unordered_map<string, void*>());
         }
         for(int i = 0; i < num; i++){
             database[table][keys[i]] = records[i];
         }
+        for(auto x : database){ // outer loop of tables
+            cout << x.second.size() << endl;
+            cout << x.second.bucket_count();
+            for(auto y : x.second){ // middle loop of records
+                for(auto z : y.second){ // inner loop of fields
+                    cout << z.second << " ";
+                }
+                //cout << endl;
+            }
+        }
+        *DB = database;
+
     }
 
     // remove specific records
     int remove(string table, vector<string> keys){
         int failed = 0;
-        unordered_map<string,unordered_map<string, void*>> database = *DB;
+        unordered_map<string,unordered_map<string, unordered_map<string,string>>> database = *DB;
         for(int i = 0; i < keys.size(); i++){
             if(!database[table].erase(keys[i])){
                 failed++;
@@ -83,10 +97,10 @@ class Seq_Database {
     }
 
     // remove based on conditions
-    int remove(string table, int numCond, vector<pair<string,void*>> conditions){
+    int remove(string table, int numCond, vector<pair<string,string>> conditions){
         int count = 0;
         vector<string> toRemove;
-        unordered_map<string,unordered_map<string, void*>> database = *DB;
+        unordered_map<string,unordered_map<string, unordered_map<string,string>>> database = *DB;
         for(auto x : database[table]){
             bool rem = true;
             for(int i = 0; i < numCond; i++){
@@ -95,7 +109,7 @@ class Seq_Database {
                 // auto a = ((unordered_map<string,void*>)x.second).at(conditions[i].first);
                 
                 //unordered_map<string, void*> Y = *((unordered_map<string, void*>*) B);
-                auto field = (*((unordered_map<string, void*>*)x.second))[conditions[i].first];
+                auto field = ((unordered_map<string, string>)x.second)[conditions[i].first];
                 rem = rem && field == conditions[i].second; // THIS IS NOT RIGHT, NEED TO CASTE TO PROPER TYPES THEN DEREFERENCE
             }
             if(rem){
@@ -107,25 +121,25 @@ class Seq_Database {
     }
 
     // Qry without sort
-    vector<unordered_map<string,void*>> get(string table, vector<pair<string,void*>> conditions){
+    vector<unordered_map<string,string>> get(string table, vector<pair<string,string>> conditions){
         int count = 0;
-        vector<unordered_map<string,void*>>* finalQry = new vector<unordered_map<string,void*>>();
-        unordered_map<string,unordered_map<string, void*>> database = *DB;
+        vector<unordered_map<string, string>>* finalQry = new vector<unordered_map<string, string>>();
+        unordered_map<string,unordered_map<string, unordered_map<string,string>>> database = *DB;
         for(auto x : database[table]) {
             bool add = true;
             for(int i = 0; i < conditions.size(); i++) {
-                auto field = (*((unordered_map<string, void*>*)x.second))[conditions[i].first];
+                auto field = ((unordered_map<string, string>)x.second)[conditions[i].first];
                 add = add && field == conditions[i].second; // THIS IS NOT RIGHT, NEED TO CASTE TO PROPER TYPES THEN DEREFERENCE
             }
             if(add) {
                 count++;
-                finalQry->push_back((*((unordered_map<string, void*>*)x.second)));
+                finalQry->push_back(x.second);
             }
         }
         return *finalQry;
     }
 
-    static bool compAsc(unordered_map<string, void*> a, unordered_map<string, void*> b){ //IS THIS RIGHT? NEED TO DEREFERENCE?
+    /*static bool compAsc(unordered_map<string, void*> a, unordered_map<string, void*> b){ //IS THIS RIGHT? NEED TO DEREFERENCE?
         string sortKey = *((unordered_map<string, void*>)a)["sort"];
         if(a[sortKey] < b[sortKey]){
             return true;
@@ -185,6 +199,22 @@ class Seq_Database {
         }
         sort(finalQry.begin(), finalQry.end(), compDesc);
         return finalQry;
+    }*/
+
+    void printDB(){
+        unordered_map<string,unordered_map<string, unordered_map<string,string>>> database = *DB;
+        cout << "print all the rows in DB" << endl;
+        cout << database.size() << endl;
+        for(auto x : database){ // outer loop of tables
+            cout << x.second.size() << endl;
+            cout << x.second.bucket_count();
+            for(auto y : x.second){ // middle loop of records
+                for(auto z : y.second){ // inner loop of fields
+                    // cout << z.second << " ";
+                }
+                //cout << endl;
+            }
+        }
     }
 
 };
--- src/test_db.cpp
+++ src/test_db.cpp
@@ -12,34 +12,32 @@
 using namespace std;
 
 bool testBulkInsert(Seq_Database* DB) {
-    std::ifstream file("../data/spreadspoke_scores.csv");
-    std::string tmp; // temporary line (row)
+    std::ifstream file("../data/spreadspoke_scores.csv"); // import csv
     vector<string> games; // vec of games
     char delim = '\n'; // delimiter by newline
+    int id = 0; // temporary way of tracking index of unique row
+    std::string row; 
 
-
-    while (std::getline(file, tmp, delim)) {
-        games.push_back(tmp);
+    while (std::getline(file, row, delim)) {
+        games.push_back(row);
     }
 
-    int id = 0;
-
-    vector<unordered_map<string, string>> *ins_vec = new vector<unordered_map<string, string>>(); // outermost
-    vector<int> keys = new vector<int>(); // list keys
+    vector<unordered_map<string, string>> *ins_vec = new vector<unordered_map<string, string>>(); // vector to insert
+    vector<string> *keys = new vector<string>(); // list keys
 
     for(auto it = games.begin() + 1; it != games.end(); it++) { 
         id += 1;
-        std::cout << *it << std::endl;
-        // char column_field = strtok(*it, ","); // tokenize the game row
-        // while(column_field != NULL) {
+        // std::cout << *it << std::endl;
+        string s = "";
+        s += id;
+        keys->push_back(s);
         unordered_map<string, string> *ins_row = new unordered_map<string, string>();
-        keys->push_back(id);
-            
-        // }
-        ins_row["raw-data"] = *it;
-        ins_vec->push_back(ins_row);        
+        string rd = "raw-data";
+        (*ins_row)[rd] = *it;
+        ins_vec->push_back(*ins_row);        
     }
-    // DB->insert("games", ins_vec.size(), *keys, *ins_vec);
+    DB->insert("games", ins_vec->size(), *keys, *ins_vec);
+    DB->printDB();
     return true;
 }
 
