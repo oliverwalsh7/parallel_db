d3b576dfff453b0b9663a4d7833e0cfd04d937e7
Justin Angelel <jangelel17@icloud.com>
2021-12-12 13:32:54 -0500

    Sequential Code, fixing comp erros

--- src/SequentialDB.cpp
+++ src/SequentialDB.cpp
@@ -16,7 +16,12 @@ using namespace std;
 
 class Seq_Database {
     private:
+    // Outer Level Hashmap of tables
+    //   Second Level Hashmap of records in "table"
+    //     Third Level Hashmap where each map is a record
     unordered_map<string, unordered_map<string,void*>>* DB;
+    //unordered_map<string, unordered_map<string, unordered_map<string,void*>>>* DB;
+    //string sortKey;
 
     class compObj{
         private:
@@ -47,27 +52,30 @@ class Seq_Database {
 
     Seq_Database(){
         DB = new unordered_map<string, unordered_map<string,void*>>();
+        //sortKey = "";
     }
 
-    bool insert(string table, int num, vector<string> keys, vector<void*> records){
+    //Change to bool/int for failed insertions?
+    void insert(string table, int num, vector<string> keys, vector<void*> records){
+        unordered_map<string,unordered_map<string, void*>> database = *DB;
         try {
             DB->at(table);
         } catch (const std::out_of_range& oor) {
-            unordered_map<string, void*> database = *DB;
-            database[table] = new unordered_map<string, void*>();
+            unordered_map<string, void*>* x = new unordered_map<string, void*>();
+            database[table] = *x;
             //DB->insert(std::make_pair<string, unordered_map<string, void*>(table, new unordered_map<string, void*>());
         }
         for(int i = 0; i < num; i++){
-            DB[table][keys[i]] = records[i];
+            database[table][keys[i]] = records[i];
         }
     }
 
-    /*
     // remove specific records
     int remove(string table, vector<string> keys){
         int failed = 0;
+        unordered_map<string,unordered_map<string, void*>> database = *DB;
         for(int i = 0; i < keys.size(); i++){
-            if(!DB[table].erase(keys[i])){
+            if(!database[table].erase(keys[i])){
                 failed++;
             }
         }
@@ -75,81 +83,108 @@ class Seq_Database {
     }
 
     // remove based on conditions
-    int remove(string table, int numCond, vector<void*> conditions){
+    int remove(string table, int numCond, vector<pair<string,void*>> conditions){
         int count = 0;
         vector<string> toRemove;
-        for(x : DB[table]){
-            remove = true;
+        unordered_map<string,unordered_map<string, void*>> database = *DB;
+        for(auto x : database[table]){
+            bool rem = true;
             for(int i = 0; i < numCond; i++){
-                y = (unordered_map<string,void*>) x->second;
-                remove = remove && y[conditions[i][0]]==conditions[i][1]];
+                //unordered_map<string,void*>* y = (unordered_map<string,void*>) x.second;
+                // void* y = (.second;
+                // auto a = ((unordered_map<string,void*>)x.second).at(conditions[i].first);
+                
+                //unordered_map<string, void*> Y = *((unordered_map<string, void*>*) B);
+                auto field = (*((unordered_map<string, void*>*)x.second))[conditions[i].first];
+                rem = rem && field == conditions[i].second; // THIS IS NOT RIGHT, NEED TO CASTE TO PROPER TYPES THEN DEREFERENCE
             }
-            if(remove){
+            if(rem){
                 count++;
-                toRemove.push_back(x->first);
+                toRemove.push_back(x.first);
             }
         }
         return count - remove(table, toRemove);
     }
 
     // Qry without sort
-    vector<void*> get(string table, vector<void*> conditions){
+    vector<unordered_map<string,void*>> get(string table, vector<pair<string,void*>> conditions){
         int count = 0;
-        vector<void*> finalQry = new vector<void*>();
-        for(x : DB[table]) {
-            add = true;
-            for(int i = 0; i < numCond; i++) {
-                y = (unordered_map<string,void*>) x->second;
-                add = add && y[conditions[i][0]]==conditions[i][1]];
+        vector<unordered_map<string,void*>>* finalQry = new vector<unordered_map<string,void*>>();
+        unordered_map<string,unordered_map<string, void*>> database = *DB;
+        for(auto x : database[table]) {
+            bool add = true;
+            for(int i = 0; i < conditions.size(); i++) {
+                auto field = (*((unordered_map<string, void*>*)x.second))[conditions[i].first];
+                add = add && field == conditions[i].second; // THIS IS NOT RIGHT, NEED TO CASTE TO PROPER TYPES THEN DEREFERENCE
             }
             if(add) {
                 count++;
-                finalQry.push_back(x->second);
+                finalQry->push_back((*((unordered_map<string, void*>*)x.second)));
             }
-        };
-        return finalQry;
+        }
+        return *finalQry;
     }
 
-    // bool compAsc(void* a, void* b, void* key){
-    //     sortKey = a["sort"];
-    //     if(a[sortKey] < b[sortKey]){
-    //         return true;
-    //     }
-    //     return false;
-    // }
+    static bool compAsc(unordered_map<string, void*> a, unordered_map<string, void*> b){ //IS THIS RIGHT? NEED TO DEREFERENCE?
+        string sortKey = *((unordered_map<string, void*>)a)["sort"];
+        if(a[sortKey] < b[sortKey]){
+            return true;
+        }
+        return false;
+    }
 
-    // bool compDesc(void* a, void* b){
-    //     sortKey = a["sort"];
-    //     if(a[sortKey] < b[sortKey]){
-    //         return false;
-    //     }
-    //     return true;
-    // }
+    static bool compDesc(unordered_map<string, void*> a, unordered_map<string, void*> b){ //IS THIS RIGHT? NEED TO DEREFERENCE?
+        if(a[sortKey] < b[sortKey]){
+            return false;
+        }
+        return true;
+    }
 
     // Qry with sort
-    vector<void*> get(string table, vector<void*> conditions, void* sort, bool ascend = true){
+    vector<unordered_map<string, void*>> get(string table, vector<pair<string,void*>> conditions, string sort, bool ascend = true){
+        // int count = 0;
+        // vector<void*>* finalQry = new vector<void*>();
+        // for(auto x : DB[table]){
+        //     add = true;
+        //     for(int i = 0; i < conditions.size(); i++){
+        //         y = (unordered_map<string,void*>) x->second;
+        //         add = add && y[conditions[i][0]]==conditions[i][1];
+        //     }
+        //     if(add){
+        //         count++;
+        //         //string s = "sort";
+        //         //IF THE COMP OBJECT WORKS THIS IS NOT NECESSARY
+        //         //((unordered_map<string,void*>) x->second)["sort"] = sort;
+        //         finalQry.push_back(x->second);
+        //     }
+        // }
         int count = 0;
-        vector<void*> finalQry = new vector<void*>();
-        for(x : DB[table]){
-            add = true;
-            for(int i = 0; i < numCond; i++){
-                y = (unordered_map<string,void*>) x->second;
-                add = add && y[conditions[i][0]]==conditions[i][1];
+        vector<unordered_map<string,void*>>* finalQry = new vector<unordered_map<string,void*>>();
+        unordered_map<string,unordered_map<string, void*>> database = *DB;
+        for(auto x : database[table]) {
+            bool add = true;
+            for(int i = 0; i < conditions.size(); i++) {
+                auto field = (*((unordered_map<string, void*>*)x.second))[conditions[i].first];
+                add = add && field == conditions[i].second; // THIS IS NOT RIGHT, NEED TO CASTE TO PROPER TYPES THEN DEREFERENCE
             }
-            if(add){
+            if(add) {
                 count++;
                 string s = "sort";
-                ((unordered_map<string,void*>) x->second)["sort"] = sort;
-                finalQry.push_back(x->second);
+                //IF THE COMP OBJECT WORKS THIS IS NOT NECESSARY
+                (*((unordered_map<string, void*>*)x.second))["sort"] = sort;
+                finalQry->push_back((*((unordered_map<string, void*>*)x.second)));
             }
         }
-        compObj comparator(sort, ascend);
+        //vector<unordered_map<string, void*>> finalQry = get(table, conditions);
+
+        // compObj comparator(sort, ascend);
+        //sortKey = sort;
         if (ascend){
-            sort(finalQry.begin(), finalQry.end(), compObj);
+            sort(finalQry.begin(), finalQry.end(), compAsc);
             return finalQry;
         }
-        sort(finalQry.begin(), finalQry.end(), compObj);
+        sort(finalQry.begin(), finalQry.end(), compDesc);
         return finalQry;
     }
-*/
+
 };
